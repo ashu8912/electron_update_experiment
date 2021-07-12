07171c7e9aa0933df594bc76f8d8c5309f224090
Ashu Ghildiyal <ashu@kinvolk.io>
2021-07-12 20:40:42 +0530

    add more channels to electron main context

--- preload.js
+++ preload.js
@@ -9,13 +9,13 @@ contextBridge.exposeInMainWorld(
   "api", {
       send: (channel, data) => {
           // whitelist channels
-          let validChannels = ["toMain"];
+          let validChannels = ["update_available", "update_downloaded"];
           if (validChannels.includes(channel)) {
               ipcRenderer.send(channel, data);
           }
       },
       receive: (channel, func) => {
-          let validChannels = ["app_version"];
+          let validChannels = ["app_version","update_available", "update_downloaded"];
           if (validChannels.includes(channel)) {
               // Deliberately strip event as it includes `sender` 
               ipcRenderer.on(channel, (event, ...args) => func(...args));
